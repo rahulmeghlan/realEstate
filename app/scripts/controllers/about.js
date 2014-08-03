ce4043d90804687111b72b4aeac7f4e48b59b2f9
Rahul Meghlan <rahulmeghlan@gmail.com>
2014-08-03 10:01:24 +0530

    Scaffolded the project using yeoman-angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name realEstateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the realEstateApp
+ */
+angular.module('realEstateApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name realEstateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the realEstateApp
+ */
+angular.module('realEstateApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
